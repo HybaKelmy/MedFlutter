4ade9c3b62ec4bd52cb5dc03234f4fa77b58b01c
HybaKelmy <hyba.kelmi@gmail.com>
2021-03-21 14:28:16 +0100

    yeah this is first commit

---
+++ lib/screens/onboarding.dart
@@ -0,0 +1,96 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+import 'package:argon_flutter/constants/Theme.dart';
+
+class Onboarding extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(children: <Widget>[
+      Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/img/bb.jpg"),
+                  fit: BoxFit.cover))),
+      Padding(
+        padding:
+            const EdgeInsets.only(top: 73, left: 32, right: 32, bottom: 16),
+        child: Container(
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Image.asset("assets/img/logowhite.png", scale: 0.5),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+
+                    Padding(
+                      padding: const EdgeInsets.only(top: 2.0),
+                      child: Text("Garder votre santé, c'est notre mission. ",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w200)),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 1.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: FlatButton(
+                      textColor: ArgonColors.text,
+                      color: ArgonColors.secondary,
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, '/home');
+                      },
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4.0),
+                      ),
+                      child: Padding(
+                          padding: EdgeInsets.only(
+                              left: 16.0, right: 16.0, top: 12, bottom: 12),
+                          child: Text("LOGIN",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16.0)
+                          )
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 1.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: FlatButton(
+                      textColor: ArgonColors.text,
+                      color: ArgonColors.secondary,
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, '/profile');
+                      },
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4.0),
+                      ),
+                      child: Padding(
+                          padding: EdgeInsets.only(
+                              left: 16.0, right: 16.0, top: 12, bottom: 12),
+                          child: Text("SIGNUP",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16.0))),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      )
+    ]));
+  }
+}
